893c3c14fa365fe8a6f9be7ad5a2f88ce3e384f0
tungpv <tungpv@mirabo-global.com>
2025-08-07 16:17:39 +0700

    feat: update user, ant toast

--- src/utils/dateHelper.ts
+++ src/utils/dateHelper.ts
@@ -4,7 +4,7 @@ export function formatDate(dateInput: string | Date): string {
   const formatted = dayjs(dateInput).format("DD/MM/YYYY");
   return formatted === "Invalid Date" ? "Invalid date" : formatted;
 }
-export function parseDate(dateString: string): Date | null {
+export function parseDate(dateString: string): dayjs.Dayjs | null {
   const parsed = dayjs(dateString, "DD/MM/YYYY");
-  return parsed.isValid() ? parsed.toDate() : null;
+  return parsed.isValid() ? parsed : null;
 }
