40283d562325883f473c52b087808a4243957abb
tungpv <tungpv@mirabo-global.com>
2025-08-09 22:39:15 +0700

    feat: best seller book

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,14 @@
+import TopBestSellerBook from "@/layouts/user/home_page/TopBestSellerBook.tsx";
+import RelatedSearches from "@/layouts/user/home_page/RelatedSearches.tsx";
 
 
 const HomePage = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <>HOMEPAGE</>
+        <>HOMEPAGE</>
+
+        <RelatedSearches />
+        <TopBestSellerBook />
     </div>
   );
 };
